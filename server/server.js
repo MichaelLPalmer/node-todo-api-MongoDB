8c61e3c1ff82bf7738d03786524edc16521127ba
MichaelLPalmer <michael.palmer.12@cnu.edu>
2017-08-29 17:08:29 -0400

    Added GET /todos/:id.

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {Users} = require('./models/users');
 
+var {ObjectID} = require('mongodb');
+
 const port = process.env.PORT || 3000;
 var app = express();
 
@@ -27,6 +29,21 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id', (req,res) => {
+	var id = req.params.id;
+	if (!ObjectID.isValid(id)){
+		console.log('ID not valid.');
+		return res.status(404).send();
+	};
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			console.log('Todo not found.');
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => res.status(400).send());
 });
 
 app.listen(port, () => {
